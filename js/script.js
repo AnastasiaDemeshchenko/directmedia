08abef7af19b5964936bd0860bc4f1999de3af53
Anastasia Demeshchenko <91696766+AnastasiaDemeshchenko@users.noreply.github.com>
2021-10-28 18:33:26 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,22 +44,22 @@ $(function () {
 
 // Шапка
   
-  $(document).ready(function(){ //Вызов функции по загрузке интерфейса
-    var tempScrollTop, currentScrollTop = $(window).scrollTop(); //объявление переменных и присвоение им значений
-    $(window).scroll(function(){ //Вызов функции при прокрутке страницы
-      currentScrollTop = $(window).scrollTop(); //присвоение переменной нового значения
-        if (currentScrollTop > $('header').height()) { //Проверка условия 'переменная больше высоты шапки'
-          $('body').addClass('fixed-header'); // создание класса 'fixed-header' в селекторе 'body'
-          if ( tempScrollTop > currentScrollTop ) { //Проверка условия 'значение переменной до вызова функции больше значения после её вызова'
-            $('header').addClass('show'); //создание класса 'show' в селекторе 'header'
-          } else { // выполнение, если второе условие не прошло проверку
-            $('header').removeClass('show'); //удаление класса 'show' в селекторе 'header'
+  $(document).ready(function(){
+    var tempScrollTop, currentScrollTop = $(window).scrollTop();
+    $(window).scroll(function(){
+      currentScrollTop = $(window).scrollTop();
+        if (currentScrollTop > $('header').height()) {
+          $('body').addClass('fixed-header');
+          if ( tempScrollTop > currentScrollTop ) {
+            $('header').addClass('show');
+          } else {
+            $('header').removeClass('show');
           }
-        } else { // выполнение, если первое условие не прошло проверку
-          $('body').removeClass('fixed-header'); // удаление класса 'fixed-header' в селекторе 'body'
-          $('header').removeClass('show'); //удаление класса 'show' в селекторе 'header'
+        } else {
+          $('body').removeClass('fixed-header');
+          $('header').removeClass('show');
         }
-          tempScrollTop = currentScrollTop; //присвоение одной переменной значение другой
+          tempScrollTop = currentScrollTop;
     });
   });
 
